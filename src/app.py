e2f4de4e3727439c3d9b648f2a8a6042aaf5e3eb
Natete-Verite <nateteverite@gmail.com>
2022-06-07 17:46:38 +0300

    Added the shecodes python assessment

--- src/app.py
+++ src/app.py
@@ -4,9 +4,18 @@ import os
 
 def prime(s):
     # your code goes here
+      if s> 1:  
+        for n in range(2,s):  
+            if (s % n) == 0:  
+                return False
+        return True
+print(prime(64))
+print(prime(5))
+
 
 def solution(s):
-    return prime(s)
+    return prime(23)
+solution(23)
 
 
 if __name__ == "__main__":
